d69bd500e18fa430bc8e892d5488700678735095
RihoLuik <riho.luik@vikk.ee>
2026-01-05 12:47:29 +0200

    See on esimene versioon

---
+++ lnxskript.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+# Skript linuxil skriptimis aluste ülesandeks "Skripti versioonihaldus Git ja Github näitel"
+# Autor: Riho Luik
+
+name="Riho"
+day=$(date +%d-%m-%Y)
+clock=$(date +%T)
+
+
+echo "Tere, $name! Täna on: $day, $clock"
+echo "Hea päev skriptimiseks! Kontrollime uuendusi."
+sudo apt update
+echo "Kena päeva ning edu skriptimisega!"
